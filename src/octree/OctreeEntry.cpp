26cfd281007abb7cf7b6e643cfbef4e94de006a7
RetroGamingArena <CAILLABET@MacBook-Pro-de-Julien.local>
2015-01-18 01:36:33 +0100

    Octree set optimisation

--- src/octree/OctreeEntry.cpp
+++ src/octree/OctreeEntry.cpp
@@ -10,15 +10,6 @@
 
 #include "OctreeEntry.h"
 
-/*template<typename T>
-void OctreeEntry<T>::init()
-{
-    this->nodes = new T*[2];
-    
-    this->nodes[0] = new T[2];
-    this->nodes[1] = new T[2];
-}*/
-
 template<typename T>
 void OctreeEntry<T>::split()
 {
@@ -49,27 +40,18 @@ void OctreeEntry<T>::setCube(int x, int y, int z, int size, unsigned char type)
         return;
     }
     
-    for(int i = 0; i < 2; i++)
-        for(int j = 0; j < 2; j++)
-            for(int k = 0; k < 2; k++)
-            {
-                if(   (i * (size / 2) <= x && x < (i+1) * (size / 2))
-                    && (j * (size / 2) <= y && y < (j+1) * (size / 2))
-                    && (k * (size / 2) <= z && z < (k+1) * (size / 2))
-                    )
-                {
-                    int offset_x = x - i * (size/2);
-                    int offset_y = y - j * (size/2);
-                    int offset_z = z - k * (size/2);
+    int i = !!(x & size/2);
+    int j = !!(y & size/2);
+    int k = !!(z & size/2);
+    
+    int offset_x = x - i * (size/2);
+    int offset_y = y - j * (size/2);
+    int offset_z = z - k * (size/2);
                     
-                    //offset_y == 4
-                    
-                    if(size==2)
-                        this->get(i,j,k)->setCube(i,j,k, 1, type);
-                    else
-                        this->get(i,j,k)->setCube(offset_x,offset_y,offset_z, size/2, type);
-                }
-            }
+    if(size==2)
+        this->get(i,j,k)->setCube(i,j,k, 1, type);
+    else
+        this->get(i,j,k)->setCube(offset_x,offset_y,offset_z, size/2, type);
 }
 
 template<typename T>
