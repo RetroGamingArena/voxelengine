83b201f727030221649af8cdc2b1e5a092e99173
RetroGamingArena <CAILLABET@pc3.home>
2015-01-24 16:23:08 +0100

    Geometry shader optimisation

--- shaders/geometryShader.glsl
+++ shaders/geometryShader.glsl
@@ -1,31 +1,10 @@
 #version 330 core
 
 layout (points) in;
-layout (triangle_strip, max_vertices = 36) out;
-//layout (max_vertices = 4) out;
-
-//layout(fragmentPosition) in;
-
-/*layout (location = 0) in vec3 inPosition;
-layout (location = 1) in vec2 inCoord;
-layout (location = 2) in vec3 inNormal;
-
-out vec2 vTexCoordPass;
-out vec3 vNormalPass;*/
-
-//in vec3 fragmentColor;//
-//in float fragmentAo;
-
-//in vec3 fragmentPosition[3];
-//in vec3 fragmentColor[3];
-//in float fragmentAo[3];
-
-//out vec3 _fragmentColor;
-//out float _fragmentAo;
-
+layout (triangle_strip, max_vertices = 24) out;
 
 in mat4 PVM[1];
-in vec3 vertexPosition[1];
+in vec4 vertexPosition[1];
 in vec3 fragmentColor[1];
 in float fragmentAo[1];
 
@@ -34,275 +13,212 @@ out float _fragmentAo;
 
 void main()
 {
-    float size = 0.25;
+    float size = 0.125;
     
     int i;
     
     _fragmentColor = fragmentColor[0];
     _fragmentAo = fragmentAo[0];
     
+    mat4 bottom_model = mat4( vec4(0.0,0.0,0.0,1.0), vec4(1.0,0.0,0.0,1.0), vec4(0.0,0.0,1.0,1.0), vec4(1.0,0.0,1.0,1.0) ) + mat4(vertexPosition[0], vertexPosition[0], vertexPosition[0], vertexPosition[0]);
+    mat4 top_model    = mat4( vec4(0.0,1.0,0.0,1.0), vec4(1.0,1.0,0.0,1.0), vec4(0.0,1.0,1.0,1.0), vec4(1.0,1.0,1.0,1.0) ) + mat4(vertexPosition[0], vertexPosition[0], vertexPosition[0], vertexPosition[0]);
+    
+    mat4 bottom = PVM[0]*bottom_model;
+    mat4    top = PVM[0]*top_model;
+    
     for (i = 0; i < gl_in.length(); i++)
     {
-        vec3 position = vertexPosition[0];
-        gl_Position = PVM[0]*vec4(position,1);
+        //vec4 position = vertexPosition[0];
+        _fragmentAo = 0.0;
+        gl_Position = bottom[0];//PVM[0]*(position+vec4(0.0,0.0,0.0,1.0));
         EmitVertex();
         
-        position = vertexPosition[0];
-        position.x+=size;
-        gl_Position = PVM[0]*vec4(position,1);
+        //position = vertexPosition[0];
+        //position.x+=size;
+        _fragmentAo = 0.0;
+        gl_Position = bottom[1];//PVM[0]*(position+vec4(1.0,0.0,0.0,1.0));
         EmitVertex();
 
-        position = vertexPosition[0];
-        position.z+=size;
-        gl_Position = PVM[0]*vec4(position,1);
+        //position = vertexPosition[0];
+        //position.z+=size;
+        _fragmentAo = 0.0;
+        gl_Position = bottom[2];//PVM[0]*(position+vec4(0.0,0.0,1.0,1.0));
         EmitVertex();
-        EndPrimitive();
+        //EndPrimitive();
         
         //---
         
-        position = vertexPosition[0];
-        position.x+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
+        //position = vertexPosition[0];
+        //position.x+=size;
+        
+        //_fragmentAo = 1.0;
+        _fragmentAo = 0.0;
+        gl_Position = bottom[3];//PVM[0]*(position+vec4(1.0,0.0,0.0,1.0));
+        EmitVertex();
+        EndPrimitive();
+        
+        
+        
+        //position = vertexPosition[0];
+        //position.z+=size;
+        
+        //_fragmentAo = 1.0;
+        _fragmentAo = 0.0;
+        gl_Position = bottom[0];//PVM[0]*(position+vec4(0.0,0.0,1.0,1.0));
         EmitVertex();
         
-        position = vertexPosition[0];
-        position.z+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
+        //position = vertexPosition[0];
+        //position.x+=size;
+        //position.z+=size;
+        
+        //_fragmentAo = 1.0;
+        _fragmentAo = 0.0;
+        gl_Position = bottom[1];//PVM[0]*(position+vec4(1.0,0.0,1.0,1.0));
         EmitVertex();
         
-        position = vertexPosition[0];
-        position.x+=size;
-        position.z+=size;
+        
+        //===
+        
+        //position = vertexPosition[0];
         _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
+        gl_Position = top[0];//PVM[0]*vec4(position,1);
+        EmitVertex();
+        
+        //position = vertexPosition[0];
+        //position.x+=size;
+        _fragmentAo = 1.0;
+        gl_Position = top[1];//PVM[0]*vec4(position,1);
+        EmitVertex();
+        EndPrimitive();
+        
+        
+        
+        
+        
+        
+        
+        //position = vertexPosition[0];
+        //position.y+=size;
+        _fragmentAo = 0.0;
+        gl_Position = bottom[1];//PVM[0]*vec4(position,1);
+        EmitVertex();
+        
+        
+        //---
+        
+        //position = vertexPosition[0];
+        //position.x+=size;
+        _fragmentAo = 0.0;
+        gl_Position = bottom[3];//PVM[0]*vec4(position,1);
+        EmitVertex();
+        
+        //position = vertexPosition[0];
+        //position.y+=size;
+        _fragmentAo = 1.0;
+        gl_Position = top[1];//PVM[0]*vec4(position,1);
+        EmitVertex();
+        
+        //position = vertexPosition[0];
+        //position.x+=size;
+        //position.y+=size;
+        _fragmentAo = 1.0;
+        gl_Position = top[3];//PVM[0]*vec4(position,1);
         EmitVertex();
         EndPrimitive();
         
         //===
         
-        position = vertexPosition[0];
+        //position = vertexPosition[0];
+        //position.x+=size;
         _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
+        gl_Position = bottom[2];//PVM[0]*vec4(position,1);
         EmitVertex();
         
-        position = vertexPosition[0];
-        position.x+=size;
+        //position = vertexPosition[0];
+        //position.x+=size;
+        //position.z+=size;
         _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
+        gl_Position = bottom[3];//PVM[0]*vec4(position,1);
         EmitVertex();
         
-        position = vertexPosition[0];
-        position.y+=size;
+        //position = vertexPosition[0];
+        //position.x+=size;
+        //position.y+=size;
+        //position.z+=size;
         _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
+        gl_Position = top[2];//PVM[0]*vec4(position,1);
         EmitVertex();
-        EndPrimitive();
+        //EndPrimitive();
         
         //---
         
-        position = vertexPosition[0];
-        position.x+=size;
-        _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.y+=size;
+        //position = vertexPosition[0];
+        //position.x+=size;
         _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.x+=size;
-        position.y+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
+        gl_Position = top[3];//PVM[0]*vec4(position,1);
         EmitVertex();
         EndPrimitive();
         
-        //===
         
-        position = vertexPosition[0];
-        position.x+=size;
+        
+    
+        //position = vertexPosition[0];
+        //position.x+=size;
+        //position.y+=size;
+        //position.z+=size;
         _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
+        gl_Position = bottom[0];//PVM[0]*vec4(position,1);
         EmitVertex();
         
-        position = vertexPosition[0];
-        position.x+=size;
-        position.z+=size;
+        //position = vertexPosition[0];
+        //position.x+=size;
+        //position.y+=size;
         _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
+        gl_Position = bottom[2];//PVM[0]*vec4(position,1);
         EmitVertex();
+        //EndPrimitive();
         
-        position = vertexPosition[0];
-        position.x+=size;
-        position.y+=size;
-        position.z+=size;
+        //position = vertexPosition[0];
+        //position.z+=size;
         _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
+        gl_Position = top[0];//PVM[0]*vec4(position,1);
+        EmitVertex();
+        
+        //position = vertexPosition[0];
+        //position.x+=size;
+        //position.z+=size;
+        _fragmentAo = 1.0;
+        gl_Position = top[2];//PVM[0]*vec4(position,1);
         EmitVertex();
         EndPrimitive();
+        
+        
+         
+        //vec4 position = vertexPosition[0];
+        _fragmentAo = 1.0;
+        gl_Position = top[0];//PVM[0]*(position+vec4(0.0,0.0,0.0,1.0));
+        EmitVertex();
+        
+        //position = vertexPosition[0];
+        //position.x+=size;
+        _fragmentAo = 1.0;
+        gl_Position = top[1];//PVM[0]*(position+vec4(1.0,0.0,0.0,1.0));
+        EmitVertex();
+        
+        //position = vertexPosition[0];
+        //position.z+=size;
+        _fragmentAo = 1.0;
+        gl_Position = top[2];//PVM[0]*(position+vec4(0.0,0.0,1.0,1.0));
+        EmitVertex();
+        //EndPrimitive();
         
         //---
         
-        position = vertexPosition[0];
-        position.x+=size;
-        _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
+        //position = vertexPosition[0];
+        //position.x+=size;
         
-        position = vertexPosition[0];
-        position.x+=size;
-        position.y+=size;
-        position.z+=size;
         _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.x+=size;
-        position.y+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        EndPrimitive();
-        
-        position = vertexPosition[0];
-        position.z+=size;
-        _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.x+=size;
-        position.z+=size;
-        _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.y+=size;
-        position.z+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        EndPrimitive();
-        
-        //---
-        
-        position = vertexPosition[0];
-        position.x+=size;
-        position.z+=size;
-        _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.y+=size;
-        position.z+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.x+=size;
-        position.y+=size;
-        position.z+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        EndPrimitive();
-        
-        //===
-        
-        position = vertexPosition[0];
-        _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.z+=size;
-        _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.y+=size;
-        position.z+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        EndPrimitive();
-        
-        //---
-        
-        position = vertexPosition[0];
-        _fragmentAo = 0.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.y+=size;
-        position.z+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.y+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        EndPrimitive();
-        
-        //===
-        
-        position = vertexPosition[0];
-        position.y+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.x+=size;
-        position.y+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.y+=size;
-        position.z+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        EndPrimitive();
-        
-        //---
-        
-        position = vertexPosition[0];
-        position.x+=size;
-        position.y+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.y+=size;
-        position.z+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
-        EmitVertex();
-        
-        position = vertexPosition[0];
-        position.x+=size;
-        position.y+=size;
-        position.z+=size;
-        _fragmentAo = 1.0;
-        gl_Position = PVM[0]*vec4(position,1);
+        gl_Position = top[3];//PVM[0]*(position+vec4(1.0,0.0,0.0,1.0));
         EmitVertex();
         EndPrimitive();
     }
